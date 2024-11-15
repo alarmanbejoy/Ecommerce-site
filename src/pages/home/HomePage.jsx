42e9d95876a1430e62e37b17d82b3b1a6a3ecb41
alarmanbejoy <alarmanbejoy@gmail.com>
2024-11-16 02:31:44 +0600

    2ed commit

--- src/pages/home/HomePage.jsx
+++ src/pages/home/HomePage.jsx
@@ -3,6 +3,8 @@ import Layout from '../../components/layout/Layout';
 import HeroSection from '../../components/heroSection/HeroSection';
 import Category from '../../components/category/Category';
 import HomePageProductCard from '../../components/homePageProductCard/HomePageProductCard';
+import Track from '../../components/track/Track';
+import Testimonial from '../../components/testimonial/Testimonial';
 
 
 const HomePage = () => {
@@ -13,6 +15,8 @@ const HomePage = () => {
                 <HeroSection/>
                 <Category/>
                 <HomePageProductCard/>
+                <Track/>
+                <Testimonial/>
 
            
 
